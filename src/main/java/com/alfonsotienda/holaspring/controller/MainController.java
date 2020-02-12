d397ac337c54c227156dda3ca52c060ccca48b25
Alfonso <you@example.com>
2020-02-12 10:28:12 +0100

    ultima version hellospring

--- src/main/java/com/alfonsotienda/holaspring/controller/MainController.java
+++ src/main/java/com/alfonsotienda/holaspring/controller/MainController.java
@@ -3,6 +3,8 @@ package com.alfonsotienda.holaspring.controller;
 import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 
 import org.aspectj.apache.bcel.classfile.Module.Require;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -86,5 +88,154 @@ public class MainController {
             return "No coinciden el operador";
         }
         return operando1 + " " + operacion + " " + operando2 + " = " + resultado;
+    }
+
+    @GetMapping("/insertar")
+    public ModelAndView insertarHTML() {
+        ModelAndView modelAndView = new ModelAndView("insertar");
+        modelAndView.addObject("mensaje", "");
+        return modelAndView;
+    }
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @PostMapping("/insertar")
+    public ModelAndView insertarHTMLPost(@RequestParam("nombre") String nombre,
+            @RequestParam("apellidos") String apellidos, @RequestParam("edad") String edad) {
+
+        ModelAndView modelAndView = new ModelAndView("insertar");
+
+        int nfilas = jdbcTemplate.update("INSERT ignore INTO Employees(first, last, age) VALUES(?,?,?)", nombre,
+                apellidos, Integer.parseInt(edad));
+
+        String resultado = "INSERT ignore INTO Employees(first, last, age) VALUES('" + nombre + "','" + apellidos + "',"
+                + edad + ");";
+
+        modelAndView.addObject("mensaje", "INSERTADAS " + nfilas + " FILAS MEDIANTE " + resultado);
+
+        return modelAndView;
+    }
+
+    @GetMapping("/preguntas")
+    public ModelAndView preguntasHTML() {
+        ModelAndView modelAndView = new ModelAndView("preguntas");
+        modelAndView.addObject("mensaje", "");
+        return modelAndView;
+    }
+
+    @PostMapping("/preguntas")
+    public ModelAndView preguntasHTMLPost(@RequestParam("respuesta1") String respuesta1,
+            @RequestParam("respuesta2") String respuesta2, @RequestParam("respuesta3") String respuesta3) {
+
+        ModelAndView modelAndView = new ModelAndView("preguntas");
+        String resultado = "¿De que color es el caballo blanco de Santiago? " + respuesta1 + "\n¿Ultimo libro leido? "
+                + respuesta2 + "\n¿Color favorito? " + respuesta3;
+
+        modelAndView.addObject("mensaje", resultado);
+
+        return modelAndView;
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public ModelAndView testlocuraHTML() {
+        ModelAndView modelAndView = new ModelAndView("test");
+        modelAndView.addObject("mensaje", "");
+        return modelAndView;
+
+    }
+
+    @PostMapping("/test")
+    @ResponseBody
+
+    public ModelAndView testloculraHTMLPost(@RequestParam("respuesta1") String respuesta1,
+            @RequestParam("respuesta2") String respuesta2, @RequestParam("respuesta3") String respuesta3,
+            @RequestParam("respuesta4") String respuesta4, @RequestParam("respuesta5") String respuesta5) {
+
+        ModelAndView modelAndView = new ModelAndView("test");
+
+        String resultado = calcula1(respuesta1, respuesta2, respuesta3, respuesta4, respuesta5);
+
+        modelAndView.addObject("mensaje", resultado);
+
+        return modelAndView;
+
+    }
+
+    private String calcula1(String respuesta1, 
+                            String respuesta2, 
+                            String respuesta3,
+                            String respuesta4,
+                            String respuesta5) {
+
+        int suma1 = 0;
+        int suma2 = 0, suma3 = 0, suma4= 0, suma5 = 0;
+
+        // pregunta 1
+        if (respuesta1.equals("0")) {
+            suma1 = 0;
+        } else if (respuesta1.equals("1")) {
+            suma1 = 1;
+        } else if (respuesta1.equals("2")) {
+            suma1 = 2;
+        }
+
+        // pregunta 2
+        if (respuesta2.equals("0")) {
+            suma2 = 0;
+        } else if (respuesta2.equals("1")) {
+            suma2 = 1;
+        } else if (respuesta2.equals("2")) {
+            suma2 = 2;
+        }
+        // pregunta 3
+        if (respuesta3.equals("0")) {
+            suma3 = 0;
+        } else if (respuesta3.equals("1")) {
+            suma3 = 1;
+        } else if (respuesta3.equals("2")) {
+            suma3 = 2;
+        }
+        // pregunta 4
+        if (respuesta4.equals("0")) {
+            suma4 = 0;
+        } else if (respuesta4.equals("1")) {
+            suma4 = 1;
+        } else if (respuesta4.equals("2")) {
+            suma4 = 2;
+        }
+        // pregunta 5
+        if (respuesta5.equals("0")) {
+            suma5 = 0;
+        } else if (respuesta5.equals("1")) {
+            suma5 = 1;
+        } else if (respuesta5.equals("2")) {
+            suma5 = 2;
+        }
+
+        // calculo de la suma de puntos obtenidos en el test
+
+        int suma;
+        suma = suma1 + suma2 + suma3 + suma4 + suma5;
+
+        System.out.println("RESULTADOS TEST 2.0 ULTIMA GENRACION - SUPER FIVE QUESTIONS?");
+
+        System.out.println();
+
+        System.out.println("\nHas obtenido " + suma + " puntos.");
+
+        System.out.println();
+
+        // cerramos la funcion con el return de los resulatdos obtenidos segun nuestra
+        // clasificacion
+        if (suma >= 0 && suma < 4) {
+            return "estas al principio de la escala, completamente cuerdo pero MUY ABURRIDO";
+        } else if (suma >= 4 && suma < 9) {
+            return "estas en la parte media de la escala, vas en CAMINO HACIA LA LOCURA, BIEN!!!";
+        } else  {
+            return "enhorabuena ERES DIOS, eres el nuevo NORMAN BATES!!!";
+        }
+
     }
 }
